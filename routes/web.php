949a1733b18f0a7cd51b11b06451fa16fbd627c7
dinalailil <dinarullailil26@gmail.com>
2025-02-18 14:30:21 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+   return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+ });
+
+Route::get('/Selamat Datang', function () {
+    return 'Selamat Datang';
+ });
+
+Route::get('/about', function () {
+    return '2341760093 Dinarul Lailil Mubarokah';
+ });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel-'.$id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+    
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+    
+  
